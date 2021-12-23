100836ce103c5f5adafb4e73ddb01a8c816be750
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-23 11:52:04 +0100

    choice

--- js/app.js
+++ js/app.js
@@ -12,17 +12,11 @@ let Question = function (question1, question2, question3, question4, question5, 
     this.question9 = question9;
     this.question10 = question10;
 }
-let Choice = function (choice1, choice2, choice3, choice4, choice5, choice6, choice7, choice8, choice9, choice10) {
+let Choice = function (choice1, choice2, choice3) {
     this.choice1 = choice1;
     this.choice2 = choice2;
     this.choice3 = choice3;
-    this.choice4 = choice4;
-    this.choice5 = choice5;
-    this.choice6 = choice6;
-    this.choice7 = choice7;
-    this.choice8 = choice8;
-    this.choice9 = choice9;
-    this.choice10 = choice10;
+
 }
 
 
