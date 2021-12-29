3a87adf6d76997a30cb268a5e3bf4bfa9023e510
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-29 14:59:12 +0100

    json

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
-$(".questions");
-let Question = function (question1, question2, question3, question4, question5, question6, question7, question8,
-                         question9, question10) {
-    this.question1 = question1;
-    this.question2 = question2;
-    this.question3 = question3;
-    this.question4 = question4;
-    this.question5 = question5;
-    this.question6 = question6;
-    this.question7 = question7;
-    this.question8 = question8;
-    this.question9 = question9;
-    this.question10 = question10;
+window.onload = function () {
+    $.get("data.json", function (data) {
+        console.log(data);
+    })
 }
-let Choice = function (choice1, choice2, choice3) {
-    this.choice1 = choice1;
-    this.choice2 = choice2;
-    this.choice3 = choice3;
-
-}
-
-
-let question1 = new Question("Quel est le 1er long métrage d'animation de Walt Disney?")
