05d3d6bd85b579d9522fcf6c06d9d91800ce76e9
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2022-01-04 15:50:20 +0100

    recup json

--- js/app.js
+++ js/app.js
@@ -1,8 +1,82 @@
-$.ajax( {
-    url: "/json/question.json",
-    method: "GET",
-    dataType: "json"
-})
-    .done(function (response) {
-        let result = $("button");
+const question = $("h2");
+const answers = $(".answer");
+const homeScreen = $("#home");
+const gameScreen = $("#game");
+const scoreScreen = $("#score");
+const playBtn = $("#playBtn");
+const resultP = $("#result");
+const playAgainBtn = $("#playAgain");
+const details = $("#details");
+
+// $.get(url, function (question) {
+//     question.first().html(JSON.stringify(question));
+//     answer.last().html(answer);
+// })
+let questionCounter = 0;
+let score = 0;
+let wrongAnswers = [];
+
+function writeQuestion(questionNumber) {
+    $.ajax( {
+        url: "/json/question.json",
+        method: "GET",
+        dataType: "json"
     })
+        .done(function (response) {
+            question.first().html(response[questionNumber].question);
+            for (let i = 0; i < answers.length; i++) {
+                answers.eq(i).html(response[questionNumber].answer[i]);
+            }
+        })
+}
+
+
+answers.click(function () {
+
+    const userAnswer = this.innerText;
+
+    $.ajax( {
+        url: "/json/question.json",
+        method: "GET",
+        dataType: "json"
+    })
+        .done(function (response) {
+            if (userAnswer === response[questionCounter].right) {
+                score++;
+                console.log("bon : " + score);
+            }
+            else {
+                console.log("mauvais : " + score);
+                wrongAnswers.push([questionCounter, userAnswer]);
+                console.log(wrongAnswers);
+            }
+            writeQuestion(questionCounter +1);
+            questionCounter++;
+            if (!response[questionCounter]) {
+                gameScreen.css("display", "none");
+                scoreScreen.css("display", "flex");
+                resultP.text('votre score est de ' + score + " / " + response.length);
+
+                for (let wrongAnswer of wrongAnswers){
+                    details.html(details.html() + "- A la question numéro " + (wrongAnswer[0]+1).toString() + " vous avez répondu " + wrongAnswer[1] + ". La bonne réponse etait : " + response[wrongAnswer[0]].right + "<br>");
+                }
+            }
+        })
+})
+
+playBtn.click(() => {
+    homeScreen.css("display", "none");
+    gameScreen.css("display", "flex");
+    writeQuestion(0);
+})
+
+playAgainBtn.click(() => {
+    score = 0;
+    questionCounter = 0;
+    wrongAnswers = [];
+    scoreScreen.css("display", "none");
+    gameScreen.css("display", "flex");
+    writeQuestion(0);
+    console.log(details.html());
+    details.html(" ");
+})
